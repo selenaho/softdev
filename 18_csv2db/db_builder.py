8adeef86e32c85ed21661b5f98bf2d84a693001f
Selena Ho <sho30@stuy.edu>
2022-10-26 13:27:21 -0400

    updated 18

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -17,18 +17,23 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-def openCSV(csvF):
-    with open (csvF, newline='') as csvfile:
-        Return_dict_reader = csv.DictReader(csvfile)
-        #example row in students: {'name': 'alison', 'age': '23', 'id': '10'}
-        for row in Return_dict_reader:
-            print(row)
-        return Return_dict_reader
+c.executescript("""create table students(name text, age int, id int primary key);
+create table courses(code text, mark int, id int);
+""")
 
-students = openCSV("students.csv")
-courses = openCSV("courses.csv")
+with open ('students.csv', newline='') as csvfile: #people often forget to include the close line so with open prevents that from happening
+    students = csv.DictReader(csvfile)
+    #example row in students: {'name': 'alison', 'age': '23', 'id': '10'}
+    for entry in students:
+        name = entry['name']
+        age = entry['age']
+        Student_id = entry['id']
+        c.execute('"insert into students values("' + name + ',' + age + ',' Student_id + '"' ')
 
-print(students)
+    
+
+#print(students)
+
 
 command = ""          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
--- 18_csv2db/readme.md
+++ 18_csv2db/readme.md
@@ -1,2 +1,4 @@
+## Disco:
+- Instead of c.execute() you can use c.executescript to execute multiple statements at a time
 ## QCC:
 - What does newline='' do?
