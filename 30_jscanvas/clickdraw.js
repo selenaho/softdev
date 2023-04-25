dc0657d31769f20815cc9c1e394af7dd0331287b
Selena Ho <sho30@stuy.edu>
2023-04-24 20:12:58 -0400

    update

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -18,20 +18,55 @@ var toggleMode = (e) => {
     console.log(mode)
     console.log("toggling...");
     if (mode === "rect") {
-        mode = "circ"
+        mode = "circ";
+        toggle.innerText = "circ"; //changes the text in the button to circ
     }
     else {
-        mode = "rect"
+        mode = "rect";
+        toggle.innerText = "rect"; //changes the text in the button to rect
     }
-    console.log(mode)
-}
+    console.log(mode);
+};
 
 var drawRect = function(e) {
     console.log(e);
-    var mouseX = e.offsetX //gets X-coor of mouse when event is fired
+    var mouseX = e.offsetX //gets X-coor of mouse when event is fired, offsetX is the x coor in relation to the canvas
     var mouseY = e.offsetY //gets Y-coor of mouse when event is fired
-    console.log("mouseclick registered at ", mouseX, mouseY)
-    ctx.fillRect(mouseX, mouseY, 10, 10)
-}
+    console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.beginPath(); //starts/allows for new styling
+    ctx.fillStyle = "red"; //fill color to red
+    ctx.fillRect(mouseX, mouseY, 70, 100); //width and height of rectangle
+};
 
-c.addEventListener("click", drawRect) //passes the mouse event as parameter for the function
+var drawCircle = (e) => {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    var radius = 30;
+    console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.beginPath(); //starts/allows for new styling
+    ctx.fillStyle = "red"; //sets the fill color to red
+    ctx.strokeStyle = "black"; //sets the stroke (border) color to black
+    ctx.arc(mouseX, mouseY, radius, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+};
+
+var draw = (e) => {
+    if (mode==="rect") {
+        drawRect(e);
+    }
+    else {
+        drawCircle(e);
+    }
+};
+
+var wipeCanvas = () => {
+    ctx.clearRect(0, 0, c.width, c.height); //clears the entire canvas rectangle
+};
+
+var clear = document.getElementById("buttonClear"); 
+clear.addEventListener('click', ()=>{
+	wipeCanvas();
+});
+
+c.addEventListener("click", draw) //passes the mouse event as parameter for the function
